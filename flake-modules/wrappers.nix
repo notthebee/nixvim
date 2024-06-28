ad53710430632dd4d4ab19270dfc0466891ce0d7
Wolfgang <mail@weirdrescue.pw>
2024-06-28 21:45:18 +0200

    Temporarily remove checks

--- flake-modules/wrappers.nix
+++ flake-modules/wrappers.nix
@@ -28,31 +28,6 @@ in
         makeNixvimWithModuleUnfree = import ../wrappers/standalone.nix pkgsUnfree wrapperArgs;
       };
 
-      checks =
-        {
-          home-manager-module =
-            (import ../tests/modules/hm.nix {
-              inherit pkgs;
-              inherit (inputs) home-manager;
-              nixvim = self;
-            }).activationPackage;
-        }
-        // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
-          nixos-module =
-            (import ../tests/modules/nixos.nix {
-              inherit system;
-              inherit (inputs) nixpkgs;
-              nixvim = self;
-            }).config.system.build.toplevel;
-        }
-        // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
-          darwin-module =
-            (import ../tests/modules/darwin.nix {
-              inherit system;
-              inherit (inputs) nix-darwin;
-              nixvim = self;
-            }).system;
-        };
     };
 
   flake = {
